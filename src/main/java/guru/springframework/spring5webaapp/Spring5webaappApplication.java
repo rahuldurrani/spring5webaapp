43d87f46c87a8f53407eb554f5c81ffca28cec68
Rahul Durrani <rdurrani@stevens.edu>
2018-06-13 14:53:38 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/spring5webaapp/Spring5webaappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webaappApplication.class, args);
+	}
+}
